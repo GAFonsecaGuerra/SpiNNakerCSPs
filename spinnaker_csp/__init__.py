76662ad2338a9ed7aac7659a9dcc11c1208a6be1
Gabriel Fonseca <blacksax18@gmail.com>
2017-10-05 01:47:45 +0100

    Create main package and __init__.py file

---
+++ spinnaker_csp/__init__.py
@@ -0,0 +1,42 @@
+"""
+  Spiking Neural Network Solver for Constraint Satisfaction Problems.
+
+This package has been developed by Gabriel Fonseca and Steve Furber at The University of Manchester as part of the 
+paper:
+
+"Using Stochastic Spiking Neural Networks on SpiNNaker to Solve Constraint Satisfaction Problems"
+Submitted to the journal Frontiers in Neuroscience| Neuromorphic Engineering
+
+A constraint satisfaction problem (CSP) is generally described by a set of variables {X_i} taking values over a certain 
+set of domains {D_i}, over which the set of constraints {C_ij} is imposed. The problem consists in finding assignations
+for all variables X_i's so that all C_ij's are satisfied.
+
+To solve a particular CSP import the CSP class from snn_solver.py and create an instance object. To build the solver network
+you may implement methods of the CSP class 
+
+Additionally, you will be able to keep track of the network dynamics through the functions available in the analysis.py module
+e.g. an entropy vs. time plot which colourizes distinctively the regions satisfying the constraints (in Blue) and the ones violating 
+them (in Red). If an unsatisfying configuration is visited again it will be plotted in green. 
+
+Depending on the CSP, you will have a straightforward or a complicated description of the constraints, for the later
+it is advised to create a script translating the original problem to a CSP description, examples of this procedure
+have been included for:
+
+Sudoku:                      sudoku2csp.py     
+Ising Spin Systems           spin2csp.py
+Travelling Salesman Problem: tsp2csp.py                
+
+when the constraints are simple to describe, e.g. for an antiferromagnetic spin chain, we generate the constraints list
+on the example script. In the examples subdirectory of the project folder (SpiNNakerCSPs) we provide example implementations for:
+
+Coloring Map Problem:        australia_cmp.py and world_cmp.py
+Sudoku Puzzle:               sudoku_easy.py, sudoku_hard.py and escargot.py
+Spin Systems:                spin_system.py
+
+In the case of Sudoku, you will find a sudoku_puzzles.py inside the puzzles folder containing some example puzzles to be 
+imported on examples/sudoku.py
+"""
+
+from snn_creator import CSP
+
+__all__ = ['snn_creator', 'analysis','CSP']
